49ceaedc10c719379e6ee850fbc3827e225ba259
Jorge Ivan <jorgeperez@ciencias.unam.mx>
2021-07-09 01:49:38 -0500

    Agregando recursos

---
+++ cowboys/app/src/main/java/com/example/cowboys/SoundPlayer.java
@@ -0,0 +1,35 @@
+package com.example.cowboys;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.JobIntentService;
+
+public class SoundPlayer extends JobIntentService {
+
+    public static final String ACTION_FIRE = "com.example.cowboys.action.FIRE";
+    private static final byte JOB_ID = 0;
+
+    @Override
+    protected void onHandleWork(@NonNull Intent intent) {
+        String action = intent.getAction();
+        switch (action){
+            case ACTION_FIRE:
+                MediaPlayer soundPlayer = MediaPlayer.create(this,R.raw.fire);
+                soundPlayer.setOnCompletionListener(
+                        new MediaPlayer.OnCompletionListener() {
+                            @Override
+                            public void onCompletion(MediaPlayer mp) {
+                                mp.release();
+                            }
+                        }
+                );
+                soundPlayer.start();
+                break;
+            default:
+                Log.d(SoundPlayer.class.getSimpleName(), "Unrecognized action: "+action);
+        }
+    }
+}
